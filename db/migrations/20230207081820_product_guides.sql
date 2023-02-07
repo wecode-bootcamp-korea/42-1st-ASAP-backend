8961613c08ff49a34234ab91ed36bfe2663e2ba8
juseong <jjs7280@gmail.com>
2023-02-07 17:28:46 +0900

    [42기 장주성] [ADD] product_guides database schema 구축

---
+++ db/migrations/20230207081820_product_guides.sql
@@ -0,0 +1,16 @@
+-- migrate:up
+CREATE TABLE product_guides (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_id INT,
+    img_url VARCHAR(3000) NULL,
+    video_url VARCHAR(3000) NULL,
+    usage_description VARCHAR(3000) NOT NULL,
+    usage_amount VARCHAR(20) NULL,
+    texture VARCHAR(20) NULL,
+    blending VARCHAR(1000) NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT product_guides_product_id_fkey FOREIGN KEY (product_id) REFERENCES products (id)
+);
+
+-- migrate:down
+DROP TABLE product_guides;
