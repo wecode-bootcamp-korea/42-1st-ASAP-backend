b19608c20cab4b6e189bbb36c98a2595a2353ab6
juseong <jjs7280@gmail.com>
2023-02-08 00:30:27 +0900

    [42기 장주성] [MODIFY] 데이터베이스 스키마 가독성 보완 수정

--- db/migrations/20230207080521_product_stocks.sql
+++ db/migrations/20230207080521_product_stocks.sql
@@ -2,8 +2,8 @@
 CREATE TABLE product_stocks (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     qunatity INT DEFAULT 0,
-	product_id INT,
-	CONSTRAINT product_stocks_product_id_fkey FOREIGN KEY (product_id) REFERENCES products (id)
+    product_id INT,
+    CONSTRAINT product_stocks_product_id_fkey FOREIGN KEY (product_id) REFERENCES products (id)
 );
 
 -- migrate:down
--- db/migrations/20230207085223_orders_status.sql
+++ db/migrations/20230207085223_orders_status.sql
@@ -1,8 +1,8 @@
 -- migrate:up
 CREATE TABLE order_status(
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY(id)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
 );
 -- migrate:down
 DROP TABLE order_status;
