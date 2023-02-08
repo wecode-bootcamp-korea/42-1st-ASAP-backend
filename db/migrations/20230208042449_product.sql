afc9c259d9c56d2f1be09a80757c2d8e900e2599
juseong <jjs7280@gmail.com>
2023-02-08 13:25:43 +0900

    [42기 장주성] [Modify] products.sql 파일 칼럼 main->sub category로 변경

--- db/migrations/20230208042449_product.sql
+++ db/migrations/20230208042449_product.sql
@@ -6,12 +6,11 @@ CREATE TABLE products (
     description VARCHAR(3000) NOT NULL,
     main_ingredient VARCHAR(100) NOT NULL,
     ingredient VARCHAR(3000) NOT NULL,
-    main_category_id INT NULL,
+    sub_category_id INT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
-    CONSTRAINT products_main_category_id_fkey FOREIGN KEY (main_category_id) REFERENCES main_categories (id)
+    CONSTRAINT products_sub_category_id_fkey FOREIGN KEY (sub_category_id) REFERENCES sub_categories (id)
 );
 
 -- migrate:down
 DROP TABLE products;
-
