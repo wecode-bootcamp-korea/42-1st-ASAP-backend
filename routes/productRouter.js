e5a2592461e1518fe790fa78b9599f8e732c3be4
juseong <jjs7280@gmail.com>
2023-02-13 20:04:33 +0900

    [42기 장주성] [MODIFY] merge conflict 수정

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const productController = require('../controllers/productController');
+const productController = require('../controller/productController');
 
 router.get('/', productController.getProducts);
 router.get('/detail/:productId', productController.getProductById);
