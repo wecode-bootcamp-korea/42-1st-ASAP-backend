e2e828a7cfd7d538be8bd2f29aee092718208d3c
juseong <jjs7280@gmail.com>
2023-02-14 16:09:58 +0900

    [ADD] validateToken middleware 추가

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const orderController = require('../controllers/orderController');
+const { validateToken } = require('../middlewares/auth');
 
-router.post('/carts', orderController.createCart);
-router.post('/deliveries', orderController.createDelivery);
-router.post('/items/:orderId', orderController.createOrderItem);
-router.post('/:userId/:deliveryId', orderController.createOrder);
+router.post('/carts', validateToken, orderController.createCart);
+router.post('/deliveries', validateToken, orderController.createDelivery);
+router.post('/items/:orderId', validateToken, orderController.createOrderItem);
+router.post('/:userId/:deliveryId', validateToken, orderController.createOrder);
 
 module.exports = {
   router,
