3bd74214fe91c1383e0a318f5aafd733f390ab1f
juseong <jjs7280@gmail.com>
2023-02-15 10:25:19 +0900

    [Modify] schema file indentation 수정

--- db/migrations/20230214135055_order_items.sql
+++ db/migrations/20230214135055_order_items.sql
@@ -1,6 +1,5 @@
  -- migrate:up
-CREATE TABLE
-  order_items (
+CREATE TABLE order_items (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     product_option_id INT NOT NULL,
     order_id INT NOT NULL,
@@ -11,8 +10,7 @@ CREATE TABLE
     CONSTRAINT order_items_product_option_id_fkey FOREIGN KEY (product_option_id) REFERENCES product_options (id),
     CONSTRAINT order_items_order_id_fkey FOREIGN KEY (order_id) REFERENCES orders (id),
     CONSTRAINT order_items_order_status_id_fkey FOREIGN KEY (order_status_id) REFERENCES order_status (id)
-  );
+);
 
 -- migrate:down
-DROP TABLE
-  order_items;
+DROP TABLE order_items;
