738c627ea8737b8eb11474f78da3a530ac8ce8a0
juseong <jjs7280@gmail.com>
2023-02-15 20:49:42 +0900

    [42기 장주성] [modify] deliveries table 칼럼 수정

--- db/migrations/20230215114841_deliveries.sql
+++ db/migrations/20230215114841_deliveries.sql
@@ -4,16 +4,16 @@ CREATE TABLE deliveries (
     last_name VARCHAR(20) NOT NULL,
     first_name VARCHAR(20) NOT NULL,
     message VARCHAR(100) NULL,
-    country_code INT NOT NULL ,
-    phone_number VARCHAR(20) NOT NULL ,
+    country_code INT NULL ,
+    phone_number VARCHAR(20) NULL ,
     country VARCHAR(30) NOT NULL,
-    address VARCHAR(50) NOT NULL,
+    adress VARCHAR(50) NOT NULL,
     user_id INT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT delieveries_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (id)
     );
 
-
 -- migrate:down
 DROP TABLE deliveries;
+
