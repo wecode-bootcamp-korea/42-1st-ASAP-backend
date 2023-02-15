4cce64145cb77a7a5f72d46489299950fbc6a804
juseong <jjs7280@gmail.com>
2023-02-15 10:40:52 +0900

    [42기 장주성] [Modify] import module 순서 변경

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,7 @@
+const jwt = require('jsonwebtoken');
+
 const validateToken = async (req, res, next) => {
   try {
-    const jwt = require('jsonwebtoken');
     const SECRET_KEY = process.env.SECRET_KEY;
     const token = req.headers.authorization;
     const decoded = jwt.verify(token, SECRET_KEY);
