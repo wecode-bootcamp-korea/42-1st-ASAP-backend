9bc509a926dbdd17b23f54d3ddded00290346f67
juseong <jjs7280@gmail.com>
2023-02-16 16:18:06 +0900

    [42기 장주성] [modify] 에러핸들링 적용

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,54 +1,38 @@
 const cartService = require('../services/cartService');
 
-const createCart = async (req, res) => {
-  try {
-    const { productOptionId, quantity } = req.body;
-    const userId = req.user;
+const createCart = catchAsync(async (req, res) => {
+  const { productOptionId, quantity } = req.body;
+  const userId = req.user;
 
-    await cartService.createCart(userId, productOptionId, quantity);
+  await cartService.createCart(userId, productOptionId, quantity);
 
-    return res.status(201).json({ message: 'cartCreated' });
-  } catch (error) {
-    return res.status(error.code).json({ message: error.message });
-  }
-};
+  return res.status(201).json({ message: 'cartCreated' });
+});
 
-const updateCart = async (req, res) => {
-  try {
-    const { productOptionId, quantity } = req.body;
-    const userId = req.user;
+const updateCart = catchAsync(async (req, res) => {
+  const { productOptionId, quantity } = req.body;
+  const userId = req.user;
 
-    await cartService.updateCart(userId, productOptionId, quantity);
+  await cartService.updateCart(userId, productOptionId, quantity);
 
-    return res.status(200).json({ message: 'cartUpdated' });
-  } catch (error) {
-    return res.status(error.code).json({ message: error.message });
-  }
-};
+  return res.status(200).json({ message: 'cartUpdated' });
+});
 
-const deleteCart = async (req, res) => {
-  try {
-    const { productOptionId } = req.body;
-    const userId = req.user;
+const deleteCart = catchAsync(async (req, res) => {
+  const { productOptionId } = req.body;
+  const userId = req.user;
 
-    await cartService.deleteCart(userId, productOptionId);
+  await cartService.deleteCart(userId, productOptionId);
 
-    return res.status(200).json({ message: 'cartDeleted' });
-  } catch (error) {
-    return res.status(error.code).json({ message: error.message });
-  }
-};
+  return res.status(200).json({ message: 'cartDeleted' });
+});
 
-const getCart = async (req, res) => {
-  try {
-    const userId = req.user;
-    const result = await cartService.getCart(userId);
+const getCart = catchAsync(async (req, res) => {
+  const userId = req.user;
+  const result = await cartService.getCart(userId);
 
-    return res.status(200).json({ data: result });
-  } catch (error) {
-    return res.status(error.code).json({ message: error.message });
-  }
-};
+  return res.status(200).json({ data: result });
+});
 
 module.exports = {
   createCart,
