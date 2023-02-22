2a2ee584778bf61cf875405ec8d973736df646ba
juseong <jjs7280@gmail.com>
2023-02-22 15:29:07 +0900

    [42기 장주성] [add] unit-test 기능 추가

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const { createApp } = require('./app');
+
+const startServer = async () => {
+  const app = createApp();
+  const PORT = process.env.PORT;
+
+  AppDataSource.initialize()
+    .then(() => {
+      console.log('Data Source has been initialized!');
+    })
+    .catch((err) => {
+      console.error('Error during Data Source initialization', err);
+      AppDataSource.destroy();
+    });
+
+  app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}!!`);
+  });
+};
+
+startServer();
